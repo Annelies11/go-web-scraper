f801e36b6cdbfb3bbeef1a578f291ae891263c49
Annelies11 <mmahmudi1103@gmail.com>
2024-04-13 14:31:05 +0700

    kok gak knek nyapo

--- scraper.go
+++ scraper.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -16,8 +17,14 @@ type Article struct {
 	Title, Url, Lang string
 }
 
+type URL struct {
+	ID       int
+	Url      string
+	Category string
+}
+
 func main() {
-	res, err := http.Get("https://blog.saugi.me/")
+	res, err := http.Get("https://kumparan.com/channel/bola-sports")
 	if err != nil {
 		fmt.Println(err)
 		log.Fatal(err)
@@ -35,21 +42,32 @@ func main() {
 		fmt.Println(err)
 		log.Fatal(err)
 	}
-	file, err := os.Create("lists.csv")
+
+	file, err := os.Create("kumparan-urls-bola-sports.csv")
 	if err != nil {
 		log.Fatalln("Failed making csv : ", err)
 	}
 	defer file.Close()
 	writer := csv.NewWriter(file)
-	writer.Write([]string{"Judul", "Lang", "Url"})
-	rows := make([]Article, 0)
-	doc.Find(".card").Children().Each(func(i int, sel *goquery.Selection) {
-		row := new(Article)
-		row.Title = sel.Find(".card-title-sm").Text()
-		row.Url, _ = sel.Find(".card-title-sm span a").Attr("href")
-		row.Lang = sel.Find(".lang").Text()
+	writer.Write([]string{"ID", "Url", "Category"})
+
+	//rows := make([]Article, 0)
+	rows := make([]URL, 0)
+
+	maxScrap := 5
+
+	doc.Find(".sc-5mlv5q-0").Children().Each(func(i int, sel *goquery.Selection) {
+		// time.Sleep(1 * time.Second)
+		if i > maxScrap-1 {
+			return
+		}
+		row := new(URL)
+		row.ID = i + 1
+		row.Url, _ = sel.Find(".Viewweb__StyledView-sc-b0snvl-0 a").Attr("href")
+		row.Category = sel.Find(".Textweb__StyledText-sc-1ed9ao-0").Text()
 		rows = append(rows, *row)
-		writer.Write([]string{row.Title, row.Lang, row.Url})
+		writer.Write([]string{strconv.Itoa(row.ID), "https://kumparan.com" + row.Url, row.Category})
+
 	})
 	defer writer.Flush()
 	bts, err := json.MarshalIndent(rows, "", "  ")
@@ -58,6 +76,6 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Println(string(bts))
-	fmt.Println(string(bts))
+	//fmt.Println(string(bts))
 
 }
